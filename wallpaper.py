9da1f9af90ba1d321b7a3827135d9db4bf92f6ae
Devender <49671176+raoshaab@users.noreply.github.com>
2022-12-16 12:19:45 +0530

    Update wallpaper.py

--- wallpaper.py
+++ wallpaper.py
@@ -3,15 +3,12 @@ import  urllib.request
 import os
 import subprocess
 
-key='Your_Key_here'
-url = "https://api.unsplash.com/photos/random?query=wallpaper-4k&order_by=latest&orientation=landscape&client_id="+key
-data = urllib.request.urlopen(url).read().decode()
-#changes to full for 1080p resolution , raw is for original 4k or 8k  
-image = json.loads(data)['urls']['raw']
+key   = 'Your_Key_here'
+url   = "https://api.unsplash.com/photos/random?query=wallpaper-4k&order_by=latest&orientation=landscape&client_id="+key
+data  = urllib.request.urlopen(url).read().decode()
+#changes to raw for original 4k or 8k  resolution 
+image = json.loads(data)['urls']['full']
 
-
-print(image)
 path = r"C:\Windows\Temp\rao.jpg"
 urllib.request.urlretrieve(image,path)
-print("url downloaded")
-subprocess.call("%SystemRoot%/system32/WindowsPowerShell/v1.0/powershell.exe %appdata%/Win-Wallpaper/power_script.ps1", shell=True)
+subprocess.call('%SystemRoot%/system32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy RemoteSigned -file "%appdata%/Win-Wallpaper/power_script.ps1"', shell=True)
